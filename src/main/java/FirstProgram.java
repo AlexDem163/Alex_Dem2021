78b5b8289be7f596382b176ffdeda2dd5f2c84ba
AlexDem163 <Alex_dem@list.ru>
2021-11-07 08:49:24 +0400

    add Коментарии

--- src/main/java/FirstProgram.java
+++ src/main/java/FirstProgram.java
@@ -2,6 +2,18 @@ public class FirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Привет, мир123!");
+    }
 
+    /**
+     * Метод будет складываь два аргумента и возвращать результат
+     * @param arg1 первй аргумент
+     * @param arg2 второй аргумент
+     * @return результат слодения 2-х чисел
+     */
+    public static int summa(int arg1, int arg2) {
+       int result = arg1 + arg2;
+       // это однострочный коментарий
+        /* это многострочный коментарий,
+        его нужно открыть и закрыть */
     }
 }
