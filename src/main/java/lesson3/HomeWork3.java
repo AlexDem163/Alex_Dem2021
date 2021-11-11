0bcf75d84b82d69e574bd8b75d23b3998a31f94b
AlexDem163 <Alex_dem@list.ru>
2021-11-11 19:35:49 +0400

    HomeWork3

--- src/main/java/lesson3/HomeWork3.java
+++ src/main/java/lesson3/HomeWork3.java
@@ -5,26 +5,22 @@ public class HomeWork3 {
     public static void main(String[] args) {
 
         double a, b, c;
-        a = 0;
+        a = 5;
         b = -6;
-        c = 9;
-
+        c = -9;
+        double d = b * b - (4 * a * c);
         if (a == 0) {
             System.out.println("Первый коэффициент квадратного уравнения не может быть 0.");
-        }
-        double d = b * b - (4 * a * c);
-        if (d < 0) {
+        } else if (d > 0) {
+            double x1 = (-b - Math.sqrt(d)) / (2 * a);
+            double x2 = (-b + Math.sqrt(d)) / (2 * a);
+            System.out.println("Уравнение имеет два решения: " + x1 + " " + x2);
+        } else if (d == 0) {
+            double x;
+            x = -b / (2 * a);
+            System.out.println("Уравнение имеет одно решение - " + x);
+        } else if(d < 0) {
             System.out.println("Нет действительных решений уравнения");
         }
-        if (d == 0) {
-            double x12 = -b / (2 * a);
-            System.out.println("Уравнение имеет одно решение - " + x12);
-        }
-        if (d > 0) {
-            double x1 = (-b + Math.sqrt(d)) / (2 * a);
-            double x2 = (b + Math.sqrt(d)) / (2 * a);
-            System.out.println("Уравнение имеет два решения: " + x1 + " " + x2);
-        }
-            System.out.println("Дискриминант " + d);
-        }
     }
+}
